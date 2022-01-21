c5597e216017793f328f5f6c10db0dd7521ddcc9
kimleeyoung <65011438+kimleeyoung@users.noreply.github.com>
2022-01-22 04:06:19 +0900

    카카오 로그인 기능
    kakao_login_page

---
+++ main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk/all.dart';
+
+void main() {
+  KakaoContext.clientId = '28904c5e2eb14c0fca015574afa6a557';
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'KakaoLogin',
+      home: KakaoLoginPage(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
+
+class KakaoLoginPage extends StatefulWidget {
+  @override
+  _KakaoLoginPageState createState() => _KakaoLoginPageState();
+}
+
+class _KakaoLoginPageState extends State<KakaoLoginPage> {
+  Future<void> _loginButtonPressed() async {
+    String authCode = await AuthCodeClient.instance.request();
+    print(authCode);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        minimum: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: CupertinoButton(
+                onPressed: _loginButtonPressed,
+                color: Colors.yellow,
+                child: Text(
+                  '카카오 로그인',
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.black87,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
